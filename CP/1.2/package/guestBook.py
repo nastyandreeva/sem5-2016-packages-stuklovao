a5eec88498313c0bb496462e29e2a159b46cade3
stuklovao <36235933+stuklovao@users.noreply.github.com>
2018-10-26 17:21:36 +0300

    Update guestBook.py

--- CP/1.2/package/guestBook.py
+++ CP/1.2/package/guestBook.py
@@ -1,1 +1,19 @@
+class GuestBook:
+    def __init__(self):
+        self.guests = list()
 
+    def add(self, name, surname):
+        self.guests.append({"Guest name": name})
+        self.guests.append({"Guest surname": surname})
+
+    def remove(self, name, surname):
+        for guest in self.guests:
+            if (guest.get("Guest name") == name) and (guest.get("Guest surname") == surname):
+                self.guests.remove(guest)
+
+    def write_file(self):
+
+        import json
+        with open("./book.json", 'a') as file:
+            data = { "Guests": self.guests }
+            file.write(json.dumps(data,file))
